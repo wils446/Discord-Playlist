a4587c491d5f1445d952b4c58a81bf2253125e16
wils446 <wilsonlimmmm@gmail.com>
2022-03-09 09:51:34 +0700

    add error handler

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,13 +1,17 @@
 import Express from "express";
 import dotenv from "dotenv";
+import { errorHandler } from "./app/middlewares";
 
 async function main() {
 	dotenv.config({ path: ".env" });
 
 	const app = Express();
+	app.use(Express.json());
 
 	const { config } = await import("./core/config");
 	await import("./core/db");
+
+	app.use(errorHandler);
 
 	app.listen(config.PORT, () => {
 		console.log(`Server started on port ${config.PORT}`);
---
+++ server/src/libs/errors/index.ts
@@ -0,0 +1,5 @@
+export { default as BadRequest } from "./BadRequest";
+export { default as NotFoundError } from "./NotFoundError";
+export { default as PermissionError } from "./PermissionError";
+export { default as UnauthorizedError } from "./UnauthorizedError";
+export { default as ValidationError } from "./ValidationError";
