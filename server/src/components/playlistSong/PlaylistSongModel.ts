77f25f631d1f8db91bca973e6fc7435c292a466e
wils446 <wilsonlimmmm@gmail.com>
2022-03-09 16:37:33 +0700

    create PlaylistSong model

---
+++ server/src/components/playlistSong/PlaylistSongModel.ts
@@ -0,0 +1,32 @@
+import Joi from "joi";
+import { Model, Pojo, Validator, ValidatorArgs } from "objection";
+import { ValidationError } from "../../libs/errors";
+import { playlistSongModel } from "./interface";
+
+export default class PlaylistSong extends Model implements playlistSongModel {
+	public playlistId!: number;
+	public songId!: number;
+
+	static get tableName() {
+		return "playlist_song";
+	}
+
+	static createValidator(): Validator {
+		return new PlaylistSongValidator();
+	}
+}
+
+class PlaylistSongValidator extends Validator {
+	validate(args: ValidatorArgs): Pojo {
+		const data = args.json;
+
+		const schema = Joi.object({
+			playlistId: Joi.number().required(),
+			songId: Joi.number().required(),
+		});
+		const validate = schema.validate(data);
+		if (validate.error) throw new ValidationError(validate.error.details[0].message);
+
+		return data;
+	}
+}
---
+++ server/src/components/playlistSong/interface.ts
@@ -0,0 +1,4 @@
+export interface playlistSongModel {
+	playlistId: number;
+	songId: number;
+}
